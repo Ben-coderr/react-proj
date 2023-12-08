c679be8fb0ea796568b0322d4c66997e3ad41c8d
Benkorich Abdenour <benkorich.abdenour@gmail.com>
2023-12-08 17:34:55 +0100

    outputing lists

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Abdenour");
-  const HandleClick = () => {
-    setName("Youcef");
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "Joker", body: "lorem epsume hh ..", author: "somebody", id: 1 },
+    { title: "Batman", body: "lorem epsume hh ..", author: "somebody", id: 2 },
+    {
+      title: "Spider Man",
+      body: "lorem epsume hh ..",
+      author: "somebody",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="Home">
-      <h2>{name}</h2>
-      <button onClick={HandleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog" key={blog.id}>
+          <h1>{blog.title}</h1>
+          <p>{blog.body}</p>
+        </div>
+      ))}
     </div>
   );
 };
