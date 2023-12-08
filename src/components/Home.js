a41c6682425d7d31777cd8e4d361b2bec44d6416
Benkorich Abdenour <benkorich.abdenour@gmail.com>
2023-12-08 22:14:24 +0100

    useEffect()

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,13 +17,21 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  const [name, setName] = useState("abdenour");
+
   useEffect(() => {
-    console.log(blogs);
-  });
+    // only runs on re render
+    console.log("runing ..");
+    console.log({ name });
+  }, [name]);
+  // ,[] => means runs once
 
   return (
     <div className="Home">
       <Bloglist blogs={blogs} title="All Blogs" HandleBlog={HandleBlog} />
+      <button onClick={() => setName("Benkorich")}>Change name</button>
+      <p>{name}</p>
     </div>
   );
 };
